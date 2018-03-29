eed36b14d23b11ab628b48cf8514a12114ca1f97
Martin Müller <mail@mueller-martin.net>
2018-03-29 03:06:09 +0200

    Change default config to use network module

--- privacyscanner/defaultconfig.py
+++ privacyscanner/defaultconfig.py
@@ -1,6 +1,6 @@
 QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=dev-stretch'
 MAX_EXECUTION_TIMES = {None: None}
 SCAN_MODULE_OPTIONS = {}
-SCAN_MODULES = ['privacyscanner.scanmodules.example']
+SCAN_MODULES = ['privacyscanner.scanmodules.network']
 NUM_WORKERS = 2
 MAX_EXECUTIONS = 100
