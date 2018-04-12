cdfbefbee9b0899362807866d9a2ad9230c5f71d
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-04-13 00:48:48 +0200

    Change database host in defaultconfig

--- privacyscanner/defaultconfig.py
+++ privacyscanner/defaultconfig.py
@@ -1,4 +1,4 @@
-QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=dev-stretch'
+QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=localhost'
 MAX_EXECUTION_TIMES = {None: None}
 SCAN_MODULE_OPTIONS = {}
 SCAN_MODULES = ['privacyscanner.scanmodules.network', 'privacyscanner.scanmodules.openwpm',
