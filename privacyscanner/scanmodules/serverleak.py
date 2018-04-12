1bd347747c453e37e0696987ba33d54aedb7bdcd
Martin Müller <mail@mueller-martin.net>
2018-04-12 22:18:24 +0200

    serverleak: Migrate to new API + PEP 8 fixes

--- privacyscanner/scanmodules/serverleak.py
+++ privacyscanner/scanmodules/serverleak.py
@@ -12,8 +12,9 @@ from requests.models import Response
 from concurrent.futures import ThreadPoolExecutor
 
 
-test_name = 'serverleak'
-test_dependencies = []
+name = 'serverleak'
+dependencies = []
+required_keys = ['site_url']
 
 
 def _match_db_dump(content):
@@ -23,12 +24,14 @@ def _match_db_dump(content):
         matched |= target in content
     return matched
 
+
 def _concat_sub(url, suffix):
     url_extract = extract(url)
     if url_extract.subdomain == "":
         return None
     site = url_extract.subdomain + "." + url_extract.domain
     return site + suffix
+
 
 def _concat_full(url, suffix):
     url_extract = extract(url)
@@ -37,41 +40,54 @@ def _concat_full(url, suffix):
         site = url_extract.subdomain + "." + site
     return site + suffix
 
+
 def _gen_db_domain_sql(url):
     return extract(url).domain + ".sql"
+
 
 def _gen_db_sub_domain_sql(url):
     return _concat_sub(url, ".sql")
 
+
 def _gen_db_full_domain_sql(url):
     return _concat_full(url, ".sql")
+
 
 def _gen_db_domain_db(url):
     return extract(url).domain + ".db"
 
+
 def _gen_db_sub_domain_db(url):
     return _concat_sub(url, ".db")
+
 
 def _gen_db_full_domain_db(url):
     return _concat_full(url, ".db")
 
+
 def _gen_db_domain_key(url):
     return extract(url).domain + ".key"
+
 
 def _gen_db_sub_domain_key(url):
     return _concat_sub(url, ".key")
 
+
 def _gen_db_full_domain_key(url):
     return _concat_full(url, ".key")
+
 
 def _gen_db_domain_pem(url):
     return extract(url).domain + ".pem"
 
+
 def _gen_db_sub_domain_pem(url):
     return _concat_sub(url, ".pem")
 
+
 def _gen_db_full_domain_pem(url):
     return _concat_full(url, ".pem")
+
 
 TRIALS = [
     ('server-status/', 'Apache Server Status'),
@@ -81,8 +97,9 @@ TRIALS = [
     ('.git/HEAD', 'ref:'),
     ('.svn/wc.db', 'SQLite'),
     ('core', 'ELF'),
-    ### Check for Database dumps
-    # sqldump - mysql
+
+    # Check for Database dumps
+    # sqldump - MySQL/MariaDB
     ('dump.db', _match_db_dump),
     ('dump.sql', _match_db_dump),
     ('sqldump.sql', _match_db_dump),
@@ -112,9 +129,11 @@ TRIALS = [
     (_gen_db_full_domain_key, '-----BEGIN'),
     (_gen_db_domain_pem, '-----BEGIN'),
     (_gen_db_sub_domain_pem, '-----BEGIN'),
-    (_gen_db_full_domain_pem, '-----BEGIN'),
-    # TODO Add [domainname].key, [domainname].pem
+    (_gen_db_full_domain_pem, '-----BEGIN')
+
+    # TODO Add .DS_Store files for macOS
 ]
+
 
 def _get(url, timeout):
     try:
@@ -123,24 +142,38 @@ def _get(url, timeout):
     except ConnectionError:
         return None
 
-def test_site(url: str, previous_results: dict) -> Dict[str, Dict[str, Union[str, bytes]]]:
-    raw_requests = {
-        'url': {
-            'mime_type': 'text/plain',
-            'data': url.encode(),
-        }
+
+def _response_to_json(resp: Response) -> bytes:
+    """Generate a json byte string from a response
+    received through requests."""
+    # we store only the top of the file
+    # because core dumps can become very large
+    # also: we do not want to store more potentially sensitive data
+    # than necessary to determine whether there is a leak or not
+
+    return {
+        'text': resp.content[0:50*1024].decode(errors='replace'),
+        'status_code': resp.status_code,
+        'headers': dict(resp.headers),
+        'url': resp.url,
     }
 
-    # determine hostname
-    parsed_url = urlparse(url)
 
+def scan_site(result, logger, options):
+    leaks = []
+    trials = {}
+    # determine hostname
+    parsed_url = urlparse(result['site_url'])
+
+    # TODO: Get worker count from options
     with ThreadPoolExecutor(max_workers=8) as executor:
         url_to_future = {}
+
         for trial, pattern in TRIALS:
             trial_t = trial
             # Check if trial is callable. If so, call it and save the result
             if callable(trial):
-                trial_t = trial(url)
+                trial_t = trial(result['site_url'])
                 if trial_t is None:
                     continue
             request_url = '{}://{}/{}'.format(
@@ -156,41 +189,29 @@ def test_site(url: str, previous_results: dict) -> Dict[str, Dict[str, Union[str
 
                 match_url = '{}/{}'.format(parsed_url.netloc, trial)
 
-                if  match_url not in response.url:
+                if match_url not in response.url:
                     # There has been a redirect.
                     continue
-                
-                raw_requests[trial] = {
-                    'mime_type': 'application/json',
-                    'data': _response_to_json(response),
-                }
+
+                trials[trial] = _response_to_json(response)
             except Exception:
                 continue
 
-    return raw_requests
-
-
-def process_test_data(raw_data: list, previous_results: dict) -> Dict[str, Dict[str, object]]:
-    leaks = []
-    result = {}
-    
-    url = None
-    if 'url' in raw_data:
-        url = raw_data['url']['data'].decode()
-
     for trial, pattern in TRIALS:
-        if url:
-            if callable(trial):
-                trial = trial(url)
-                if trial is None:
-                    continue
-        if trial not in raw_data:
+        if callable(trial):
+            trial = trial(result['site_url'])
+            if trial is None:
+                continue
+        if trial not in trials:
             # Test raw data too old or particular request failed.
             continue
-        response = json.loads(raw_data[trial]['data'].decode())
+
+        response = trials[trial]
+
         if response['status_code'] == 200:
             # The pattern can have three different types.
-            # - If it is a simple string, we only check if it is contained in the response
+            # - If it is a simple string,
+            #   we only check if it is contained in the response
             if isinstance(pattern, str):
                 if pattern in response['text']:
                     leaks.append(trial)
@@ -198,24 +219,10 @@ def process_test_data(raw_data: list, previous_results: dict) -> Dict[str, Dict[
             elif isinstance(pattern, re._pattern_type):
                 if re.match(response['text']):
                     leaks.append(trial)
-            # - If it is callable, we call it with the response text and check the return value
+            # - If it is callable,
+            #   we call it with the response text and check the return value
             elif callable(pattern):
                 if pattern(response['text']):
                     leaks.append(trial)
 
     result['leaks'] = leaks
-    return result
-
-
-def _response_to_json(resp: Response) -> bytes:
-    """Generate a json byte string from a response received through requests."""
-    # we store only the top of the file because core dumps can become very large
-    # also: we do not want to store more potentially sensitive data than necessary
-    # to determine whether there is a leak or not
-    
-    return json.dumps({
-        'text': resp.content[0:50*1024].decode(errors='replace'),
-        'status_code': resp.status_code,
-        'headers': dict(resp.headers),
-        'url': resp.url,
-    }).encode()
