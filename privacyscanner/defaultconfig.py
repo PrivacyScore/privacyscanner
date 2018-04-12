d522c357c683ab907ff6999f7b9c25b2a1c93630
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-04-13 00:48:27 +0200

    Add serverleak and openwpm scanmodules to defaultconfig

--- privacyscanner/defaultconfig.py
+++ privacyscanner/defaultconfig.py
@@ -1,6 +1,7 @@
 QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=dev-stretch'
 MAX_EXECUTION_TIMES = {None: None}
 SCAN_MODULE_OPTIONS = {}
-SCAN_MODULES = ['privacyscanner.scanmodules.network']
+SCAN_MODULES = ['privacyscanner.scanmodules.network', 'privacyscanner.scanmodules.openwpm',
+                'privacyscanner.scanmodules.serverleaks']
 NUM_WORKERS = 2
 MAX_EXECUTIONS = 100
