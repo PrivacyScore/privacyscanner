81349107b54cf44b7575f7d2205071d3aa3da79b
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-05-26 16:53:03 +0200

    chromedevtools: Add lifetime (approximation) for the cookies

--- privacyscanner/scanmodules/chromedevtools/mixins/cookies.py
+++ privacyscanner/scanmodules/chromedevtools/mixins/cookies.py
@@ -3,4 +3,11 @@ from ..base import AbstractChromeScan
 
 class CookieMixin(AbstractChromeScan):
     def _extract_cookies(self):
-        self.result['cookies'] = self.tab.Network.getAllCookies()['cookies']
+        cookies = self.tab.Network.getAllCookies()['cookies']
+        timestamp = int(self.scan_start.timestamp())
+        for cookie in cookies:
+            if cookie['session']:
+                cookie['lifetime'] = -1
+            else:
+                cookie['lifetime'] = cookie['expires'] - timestamp
+        self.result['cookies'] = cookies
