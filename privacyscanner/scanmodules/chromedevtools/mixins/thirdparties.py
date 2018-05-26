8af8fe0c708412a23e22bf81f193997d056eea41
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-05-26 21:58:47 +0200

    chromedevtools: Improve data structure for ThirdPartiesMixin

--- privacyscanner/scanmodules/chromedevtools/mixins/thirdparties.py
+++ privacyscanner/scanmodules/chromedevtools/mixins/thirdparties.py
@@ -6,8 +6,9 @@ from ..base import AbstractChromeScan
 class ThirdPartyMixin(AbstractChromeScan):
     def _extract_third_parties(self):
         third_parties = {
-            'http': set(),
-            'https': set()
+            'fqdns': set(),
+            'http_requests': [],
+            'https_requests': []
         }
         first_party_domains = set()
         for url in (self.result['site_url'], self.result['final_url']):
@@ -15,12 +16,13 @@ class ThirdPartyMixin(AbstractChromeScan):
             first_party_domains.add(extracted.registered_domain)
         for request in self.request_log:
             extracted_url = tldextract.extract(request['url'])
-            scheme = request['parsed_url'].scheme
-            if scheme not in ('http', 'https'):
+            parsed_url = request['parsed_url']
+            if extracted_url.registered_domain in first_party_domains:
                 continue
-            if extracted_url.registered_domain not in first_party_domains:
-                third_parties[scheme].add(extracted_url.fqdn)
-        for scheme in ('http', 'https'):
-            third_parties[scheme] = list(third_parties[scheme])
-            third_parties[scheme].sort()
+            third_parties['fqdns'].add(extracted_url.fqdn)
+            if parsed_url.scheme not in ('http', 'https'):
+                continue
+            third_parties[parsed_url.scheme + '_requests'].append(request['url'])
+        third_parties['fqdns'] = list(third_parties['fqdns'])
+        third_parties['fqdns'].sort()
         self.result['third_parties'] = third_parties
