d2ba8ae017320c335e1a47a77077339263903af2
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-05-27 13:59:23 +0200

    chromedevtools: Add more information to RequestsMixin

--- privacyscanner/scanmodules/chromedevtools/mixins/requests.py
+++ privacyscanner/scanmodules/chromedevtools/mixins/requests.py
@@ -3,9 +3,25 @@ from ..base import AbstractChromeScan
 
 class RequestsMixin(AbstractChromeScan):
     def _extract_requests(self):
-        self.result['requests'] = [{
-            'url': request['url'],
-            'is_thirdparty': request['is_thirdparty'],
-            'is_tracker': request['is_tracker']
-        } for request in self.request_log
-          if not request['url'].startswith('data:')]
+        requests = []
+
+        for request in self.request_log:
+            if request['url'].startswith('data:'):
+                continue
+            response = self.response_lookup.get(request['url'])
+            requests.append({
+                'url': request['url'],
+                'is_thirdparty': request['is_thirdparty'],
+                'is_tracker': request['is_tracker'],
+                'sets_cookie': self._get_sets_cookie(response),
+                'mime_type': response['mimeType'] if response else None,
+                'status_code': response['status'] if response else None,
+                'status_text': response['statusText'] if response else None
+            })
+        self.result['requests'] = requests
+
+    @staticmethod
+    def _get_sets_cookie(response):
+        if response is None:
+            return False
+        return 'set-cookie' in response['headers_lower']
