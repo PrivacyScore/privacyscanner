1aaf3730987bc4e3b0b089fb12e50dd09a4a35c1
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-05-27 11:52:21 +0200

    chromedevtools: Do not include data urls in request_urls

--- privacyscanner/scanmodules/chromedevtools/mixins/requests.py
+++ privacyscanner/scanmodules/chromedevtools/mixins/requests.py
@@ -3,4 +3,5 @@ from ..base import AbstractChromeScan
 
 class RequestsMixin(AbstractChromeScan):
     def _extract_requests(self):
-        self.result['requested_urls'] = [req['url'] for req in self.request_log]
+        self.result['requested_urls'] = [req['url'] for req in self.request_log
+                                         if not req['url'].startswith('data:')]
