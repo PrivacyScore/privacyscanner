838a5c503da5a57139874d8387467cbac1c801d7
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-06-14 19:36:23 +0200

    chromedevtools: Make requests mixin independent of thirdparties/trackerdetect mixins

--- privacyscanner/scanmodules/chromedevtools/mixins/requests.py
+++ privacyscanner/scanmodules/chromedevtools/mixins/requests.py
@@ -9,15 +9,20 @@ class RequestsMixin(AbstractChromeScan):
             if request['url'].startswith('data:'):
                 continue
             response = self.response_lookup.get(request['url'])
-            requests.append({
+            request_dict = {
                 'url': request['url'],
-                'is_thirdparty': request['is_thirdparty'],
-                'is_tracker': request['is_tracker'],
                 'sets_cookie': self._get_sets_cookie(response),
                 'mime_type': response['mimeType'] if response else None,
                 'status_code': response['status'] if response else None,
                 'status_text': response['statusText'] if response else None
-            })
+            }
+            # is_thirdparty is only availavle if the thirdparties mixin is enabled
+            if 'is_thirdparty' in request:
+                request_dict['is_thirdparty'] = request['is_thirdparty']
+            # is_tracker is only available if the trackerdetect mixin is enabled:w
+            if 'is_tracker' in request:
+                request_dict['is_tracker'] = request['is_tracker'],
+            requests.append(request_dict)
         self.result['requests'] = requests
 
     @staticmethod
