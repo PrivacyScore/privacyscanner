4498ac7a87943903cd3f2d0aaa2eb7059d75da1a
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-06-29 17:25:48 +0200

    Remove openwpm scan module (replaced by chromedevtools)

--- privacyscanner/defaultconfig.py
+++ privacyscanner/defaultconfig.py
@@ -1,7 +1,7 @@
 QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=localhost'
 MAX_EXECUTION_TIMES = {None: None}
 SCAN_MODULE_OPTIONS = {}
-SCAN_MODULES = ['privacyscanner.scanmodules.network', 'privacyscanner.scanmodules.openwpm',
+SCAN_MODULES = ['privacyscanner.scanmodules.network', 'privacyscanner.scanmodules.chromedevtools',
                 'privacyscanner.scanmodules.serverleaks']
 NUM_WORKERS = 2
 MAX_EXECUTIONS = 100
