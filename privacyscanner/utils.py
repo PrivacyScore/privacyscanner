3b9d53a0ab4b6966b5c06ab6f3d5879fc5365a8d
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-08-09 14:58:29 +0200

    Download dependencies with fake user agent (easylist bans Python)

--- privacyscanner/utils.py
+++ privacyscanner/utils.py
@@ -1,8 +1,13 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
+
+
+FAKE_UA = 'Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'
 
 
 def download_file(url, fileobj):
-    response = urlopen(url)
+    request = Request(url)
+    request.add_header('User-Agent', FAKE_UA)
+    response = urlopen(request)
     copy_to(response, fileobj)
 
 
