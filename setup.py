53f2afda7dfb28d75f65c52bf3f4ee70ff48f586
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-01-08 14:53:50 +0100

    Change license in setup.py to MIT

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['examples', 'tests']),
     url='https://privacyscore.org/',
-    license='GPLv3 or later',
+    license='MIT License',
     author='PrivacyScore team',
     author_email='privacyscore@informatik.uni-hamburg.de',
     description='The privacyscanner component scans websites and provides data to PrivacyScore',
