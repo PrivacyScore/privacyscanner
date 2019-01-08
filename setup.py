00f5f79eb0bb7b44e2cacc889d59eb7237d3a571
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-01-08 21:29:59 +0100

    Use setup.cfg for package configuration

--- setup.py
+++ setup.py
@@ -1,19 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='privacyscanner',
-    version='0.1',
-    packages=find_packages(exclude=['examples', 'tests']),
-    url='https://privacyscore.org/',
-    license='MIT License',
-    author='PrivacyScore team',
-    author_email='privacyscore.psi@uni-bamberg.de',
-    description='The privacyscanner component scans websites and provides data to PrivacyScore',
-    install_requires=['psycopg2-binary', 'toposort', 'dnspython', 'geoip2',
-                      'requests', 'adblockeval', 'tldextract', 'pychrome',
-                      'cryptography', 'pillow', 'psutil'],
-    entry_points="""
-    [console_scripts]
-    privacyscanner=privacyscanner.scanner:main
-    """
-)
+import setuptools
+setuptools.setup()
