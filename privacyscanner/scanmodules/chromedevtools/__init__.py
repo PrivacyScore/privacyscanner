4e72fd054a34c4ee5cedd3b3a2ac52fb65dadef1
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-02-01 23:57:13 +0100

    chromedevtools: Rescan http site with https if there is no redirect

--- privacyscanner/scanmodules/chromedevtools/__init__.py
+++ privacyscanner/scanmodules/chromedevtools/__init__.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 from privacyscanner.scanmodules import ScanModule
@@ -18,6 +19,9 @@ EXTRACTOR_CLASSES = [FinalUrlExtractor, RedirectChainExtractor, GoogleAnalyticsE
                      CookieStatsExtractor, JavaScriptLibsExtractor, ScreenshotExtractor,
                      ImprintExtractor]
 
+EXTRACTOR_CLASSES_HTTPS_RUN = [FinalUrlExtractor, TLSDetailsExtractor, CertificateExtractor,
+                               InsecureContentExtractor]
+
 
 class ChromeDevtoolsScanModule(ScanModule):
     name = 'chromedevtools'
@@ -26,7 +30,8 @@ class ChromeDevtoolsScanModule(ScanModule):
 
     def __init__(self, options):
         set_default_options(options, {
-            'disable_javascript': False
+            'disable_javascript': False,
+            'https_same_content_threshold': 0.9
         })
         super().__init__(options)
         cache_file = self.options['storage_path'] / TLDEXTRACT_CACHE_FILE
@@ -35,7 +40,30 @@ class ChromeDevtoolsScanModule(ScanModule):
     def scan_site(self, result, logger, meta):
         chrome_scan = ChromeScan(EXTRACTOR_CLASSES)
         debugging_port = self.options.get('start_port', 9222) + meta.worker_id
-        chrome_scan.scan(result, logger, self.options, meta, debugging_port)
+        content = chrome_scan.scan(result, logger, self.options, meta, debugging_port)
+        if not result['reachable']:
+            return
+        result['https']['same_content'] = None
+        result['https']['same_content_score'] = None
+        if result['site_url'].startswith('http://') and not result['https']['redirect']:
+            # Lets do another scan with https but with limited extractors.
+            # We use this to annotate the http result with TLS details and
+            # insecure content details if there is not redirect to https
+            site_url = 'https://' + result['site_url'][len('http://'):]
+            extra_result = {'site_url': site_url}
+            chrome_scan = ChromeScan(EXTRACTOR_CLASSES_HTTPS_RUN)
+            https_content = chrome_scan.scan(extra_result, logger, self.options, meta,
+                                             debugging_port)
+            if not extra_result['reachable']:
+                return
+            similarity = _calculate_jaccard_index(content, https_content)
+            same_content = similarity >= self.options['https_same_content_threshold']
+            if same_content:
+                result['insecure_content'] = extra_result['insecure_content']
+                result['https'] = extra_result['https']
+                result['https']['redirect'] = False
+            result['https']['same_content_score'] = similarity
+            result['https']['same_content'] = same_content
 
     def update_dependencies(self):
         max_age = 14 * 24 * 3600
@@ -46,3 +74,15 @@ class ChromeDevtoolsScanModule(ScanModule):
         for extractor_class in EXTRACTOR_CLASSES:
             if hasattr(extractor_class, 'update_dependencies'):
                 extractor_class.update_dependencies(self.options)
+
+
+def _calculate_jaccard_index(a: bytes, b: bytes) -> float:
+    """Calculate the jaccard similarity of a and b."""
+    pattern = re.compile(rb'[ \n]')
+    # remove tokens containing / to prevent wrong classifications for
+    # absolute paths
+    a = {token for token in pattern.split(a) if b'/' not in token}
+    b = {token for token in pattern.split(b) if b'/' not in token}
+    intersection = a.intersection(b)
+    union = a.union(b)
+    return len(intersection) / len(union)
