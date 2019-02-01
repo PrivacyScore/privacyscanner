e3b5e486a1968bdbe7669f9c2fd3c5d0a252834c
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-02-01 16:57:00 +0100

    Add NumericLock utility class

--- privacyscanner/utils.py
+++ privacyscanner/utils.py
@@ -1,8 +1,36 @@
+import os
+import errno
+import fcntl
 import time
 from urllib.request import Request, urlopen
 
 
 FAKE_UA = 'Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'
+
+
+class NumericLock:
+    def __init__(self, lock_dir):
+        self.lock_dir = lock_dir
+        self._lock_file = None
+
+    def __enter__(self):
+        i = 0
+        while True:
+            i += 1
+            try:
+                f = open(self.lock_dir / ('%d.lock' % i), 'wb')
+                fcntl.lockf(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                self._lock_file = f
+                return i
+            except OSError as e:
+                if e.errno in (errno.EACCES, errno.EAGAIN):
+                    continue
+                raise
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        fcntl.lockf(self._lock_file.fileno(), fcntl.LOCK_UN)
+        self._lock_file.close()
+        os.unlink(self._lock_file.name)
 
 
 def download_file(url, fileobj):
