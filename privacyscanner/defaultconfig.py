c336d4c59116a085d50953dc9ca456abad182f00
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-02-05 11:07:34 +0100

    Remove network scan module (has been replaced by chromedevtools and dns)

--- privacyscanner/defaultconfig.py
+++ privacyscanner/defaultconfig.py
@@ -1,8 +1,7 @@
 QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=localhost'
 MAX_EXECUTION_TIMES = {None: 300}
 SCAN_MODULE_OPTIONS = {}
-SCAN_MODULES = ['privacyscanner.scanmodules.network.NetworkScanModule',
-                'privacyscanner.scanmodules.chromedevtools.ChromeDevtoolsScanModule',
+SCAN_MODULES = ['privacyscanner.scanmodules.chromedevtools.ChromeDevtoolsScanModule',
                 'privacyscanner.scanmodules.dns.DNSScanModule',
                 'privacyscanner.scanmodules.serverleaks.ServerleaksScanModule']
 NUM_WORKERS = 2
