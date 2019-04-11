27625e375094553a5fb55ab8962903cb0a144a75
Matthias Kraus <mail@matthias-kraus.com>
2019-04-11 09:28:31 +0200

    POC: Collecting Info about Integrity Availability & Hash for each stylesheet. Still not detected if hash is false

--- privacyscanner/scanmodules/chromedevtools/extractors/sricheck.py
+++ privacyscanner/scanmodules/chromedevtools/extractors/sricheck.py
@@ -4,16 +4,20 @@ from privacyscanner.scanmodules.chromedevtools.extractors.base import Extractor
 
 ELEMENT_NODE = 3
 
+
 class SriExtractor(Extractor):
     def extract_information(self):
-        tags = []
+        integrity_elements = []
         requests = self.page.request_log
-        requests2 = self.page.document_request_log
+
+        final_sri_list = []
 
         for request in requests:
             searchterm = request['url'].rsplit('/', 1)[1]
             if searchterm != "":
                 search = self.page.tab.DOM.performSearch(query=searchterm)
+                if search['resultCount'] == 0:
+                    continue
                 results = self.page.tab.DOM.getSearchResults(
                     searchId=search['searchId'], fromIndex=0, toIndex=search['resultCount'])
                 for node_id in results['nodeIds']:
@@ -25,14 +29,34 @@ class SriExtractor(Extractor):
                             # so just ignore these cases.
                             break
                         if node['nodeType'] == ELEMENT_NODE and node['nodeName'].lower() == '#text':
-                            tags.append(node['Value'][3])
-                            break
+                            if "stylesheet" in node['nodeValue']:
+                                integrity_elements.append(node['nodeValue'])
+                                print(node['nodeValue'])
+                                break
                         node_id = node.get('parentId')
 
+        for element in integrity_elements:
+            # dict of href, bool if integrity, integrity hash
+            value_parts = element.split()
+            new_entry = {}
+            new_entry['href'] = None
+            new_entry['integrity_active'] = False
+            new_entry['integrity_hash'] = None
+            for element in value_parts:
+                if 'href=' in element:
+                    new_entry['href'] = element.split('"')[1]
+                if 'integrity' in element:
+                    new_entry['integrity_active'] = True
+                    new_entry['integrity_hash'] = element.split('"')[1]
+                # own SHA check?
+            if new_entry not in final_sri_list:
+                final_sri_list.append(new_entry)
+
+        # in here for debugging
+        requests2 = self.page.document_request_log
         failed_requests = self.page.failed_request_log
         security = self.page.security_state_log
 
-        self.result['sri-fail'] = requests
-# self.page.request_log liefert mir für jeden gemachten Request (in einer list) eine enum + eine requestId,
-# die ich in failed dann nachsehen kann. BSP für falsche SRi: 1000020549.3
+        # not active to not put useless results in json
+        # self.result['sri-fail'] = requests
 
