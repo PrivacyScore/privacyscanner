f1402a4fab4d2413d90813ea1143febf5995a5b8
Matthias Kraus <mail@matthias-kraus.com>
2019-04-29 12:05:31 +0200

    Changed generatortag to DOM.querySelectorAll

--- privacyscanner/scanmodules/chromedevtools/extractors/generatortag.py
+++ privacyscanner/scanmodules/chromedevtools/extractors/generatortag.py
@@ -19,28 +19,23 @@ class GeneratorTagExtractor(Extractor):
     def extract_information(self):
         tags = []
 
-        # Use the browsers search to search for the keywords. For each result,
-        # we walk up the DOM until we find an ``meta'' element. If this element
-        # has a generator tag, this is our product/version. Otherwise, we look for the
-        # next search result.
-        for keyword in self.GENERATOR_KEYWORDS:
-            search = self.page.tab.DOM.performSearch(query=keyword)
-            if search['resultCount'] == 0:
-                continue
-            results = self.page.tab.DOM.getSearchResults(
-                searchId=search['searchId'], fromIndex=0, toIndex=search['resultCount'])
-            for node_id in results['nodeIds']:
-                while node_id is not None:
-                    try:
-                        node = self.page.tab.DOM.describeNode(nodeId=node_id)['node']
-                    except pychrome.CallMethodException:
-                        # For some reason, nodes seem to disappear in-between,
-                        # so just ignore these cases.
-                        break
-                    if node['nodeType'] == ELEMENT_NODE and node['nodeName'].lower() == 'meta':
+        node_id = self.page.tab.DOM.getDocument()['root']['nodeId']
+        meta_node_ids = self.page.tab.DOM.querySelectorAll(nodeId=node_id, selector='meta')['nodeIds']
+
+        for node_id in meta_node_ids:
+            while node_id is not None:
+                try:
+                    node = self.page.tab.DOM.describeNode(nodeId=node_id)['node']
+                except pychrome.CallMethodException:
+                    # For some reason, nodes seem to disappear in-between,
+                    # so just ignore these cases.
+                    break
+                if node['nodeType'] == ELEMENT_NODE and node['nodeName'].lower() == 'meta':
+                    if node['attributes'][1] == 'generator':
                         tags.append(node['attributes'][3])
                         break
-                    node_id = node.get('parentId')
+                node_id = node.get('parentId')
+
 
         tags = uniquify(tags)
         generator_tags = {}
